371257ab3a9c1cf57b18b936d9aa8d0970a72013
Mucsi Attila <mucsi.attila@east-box.net>
2022-01-31 15:54:09 +0100

    vue basics, v-for, v-bind, click

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Home</h1>
+    <ul>
+      <li v-for = "task in tasks" :key = "task.id" :class = "{completed: task.completed}">
+        <input @click="changeCompleted(task.id)" type = "checkbox" :checked = task.completed>
+        {{task.name}} - {{task.due}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
+  name: 'home',
+
   components: {
-    HelloWorld,
+
   },
-};
+
+  data() {
+    return {
+      title: 'Ez a Home oldal',
+      tasks: [
+        {
+          id: 1,
+          name: 'Első teszt feladat',
+          completed: false,
+          due: '2022-02-10'
+        },
+        {
+          id: 2,
+          name: 'Második feladat',
+          completed: true,
+          due: '2022-02-15'
+        },
+        {
+          id: 3,
+          name: 'Hármas feladat',
+          completed: false,
+          due: '2022-02-20'
+        },
+        {
+          id: 4,
+          name: 'Négyes feladat',
+          completed: false,
+          due: '2022-02-25'
+        }
+      ],
+    }
+  },
+
+  computed: {
+
+  },
+
+  created() {
+
+  },
+
+  methods: {
+    changeCompleted(id) {
+      let task = this.tasks.find(task => task.id == id)
+      task.completed = !task.completed
+    },
+  },
+}
 </script>
+
+<style scoped>
+  .completed {
+    text-decoration: line-through;
+  }
+</style>
